b4e447c4b8da4c46b9abb11246005f32f9245658
Jason Hamilton-Smith <hs.jason@gmail.com>
2018-07-12 00:26:26 -0700

    Implement pushing!
    God this code is janky as all hell

--- game.py
+++ game.py
@@ -23,6 +23,71 @@ class MissionComplete(Exception):
     """We are done!"""
 
 
+def attempt_push(board, pushed, direction):
+    board = copy.deepcopy(board)
+    orig_board = copy.deepcopy(board)
+
+    if direction == 'down':
+        board_orient = reversed(list(enumerate(board)))
+    else:
+        board_orient = enumerate(board)
+
+    for y, row in board_orient:
+        if direction == 'right':
+            row_orient = reversed(list(enumerate(row)))
+        else:
+            row_orient = enumerate(row)
+
+        for x, elem in row_orient:
+            elem_id = ' '.join(elem.split()[0:2])
+            if elem_id not in pushed:
+                continue
+
+            if direction == 'left':
+                t_x, t_y = x-1, y
+            if direction == 'right':
+                t_x, t_y = x+1, y
+            if direction == 'up':
+                t_x, t_y = x, y-1
+            if direction == 'down':
+                t_x, t_y = x, y+1
+
+            target = board[t_y][t_x]
+            target_cls = target.split()[0]
+
+            if target_cls == 'space':
+                # DO the movement
+                spl = elem.split()
+                if len(spl) == 5:
+                    # We gotta relink a snake as we move it
+                    o_y, o_x = [int(s) for s in spl[3:5]]
+                    if direction == 'left':
+                        n_x, n_y = o_x-1, o_y
+                    if direction == 'right':
+                        n_x, n_y = o_x+1, o_y
+                    if direction == 'up':
+                        n_x, n_y = o_x, o_y-1
+                    if direction == 'down':
+                        n_x, n_y = o_x, o_y+1
+                    n_x, n_y = str(n_x), str(n_y)
+                    elem = ' '.join(spl[0:3] + [n_y, n_x])
+
+                board[t_y][t_x] = elem
+                board[y][x] = 'space'
+            elif target_cls in ['solid', 'spike', 'fruit']:
+                # Cant push into a solid!
+                raise IllegalMove("Cant push into solids")
+            elif target_cls in ['snake', 'block']:
+                # Recurse with more items
+                target_id = ' '.join(target.split()[0:2])
+                pushed.append(target_id)
+                return attempt_push(orig_board, pushed, direction)
+            else:
+                raise InvalidMove(f"Cant push into {target}")
+
+    return board
+
+
 def move_board_state(board, teleports, endpoint, snake, direction):
     board = copy.deepcopy(board)
     b_x, b_y = len(board[0]), len(board)
@@ -82,7 +147,19 @@ def move_board_state(board, teleports, endpoint, snake, direction):
                 board[y][x] = f'snake {snake} {segment}'
                 break
 
-    elif blocker == 'space':
+    if blocker_class == 'snake' or blocker_class == 'block':
+        # We need to calculate pushing
+        # Try to push, will raise an exception if we cannot
+        # If we can we set the blocker to space and continue
+        blocker_id = ' '.join(blocker.split()[0:2])
+        board = attempt_push(board, [blocker_id], direction)
+        if board[t_y][t_x] != 'space':
+            # Theo nly way our push suceeded yet tere is no space is
+            # if we moved (were pushed) ourselves! This is illegal!
+            raise IllegalMove()
+        blocker = 'space'
+
+    if blocker == 'space':
 
         # First check if this sapce contains the endpoint, adn do that
         # logic
@@ -106,6 +183,8 @@ def move_board_state(board, teleports, endpoint, snake, direction):
                 next_y = int(snake_parts[3])
                 next_x = int(snake_parts[4])
                 next_seg = board[next_y][next_x]
+                if int(snake_parts[2]) > 10:
+                    exit(1)
                 if len(next_seg.split()) < 5:
                     board[y][x] = f'snake {snake} {segment}'
                 else:
@@ -116,16 +195,9 @@ def move_board_state(board, teleports, endpoint, snake, direction):
                 board[y][x] = f'space'
                 break
 
-    elif blocker == 'solid' or blocker == 'spikes':
+    if blocker == 'solid' or blocker == 'spikes':
         # No can do
         raise IllegalMove()
-
-    elif blocker_class == 'snake' or blocker_class == 'block':
-        # We need to calculate pushing
-        raise NotImplemented()
-
-    else:
-        raise NotImplemented(f'blocker_class')
 
     return board, teleports, endpoint
 
