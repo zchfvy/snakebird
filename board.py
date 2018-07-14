d844037bb3cb09fda83f33035b9a3b181fbc0ff3
Jason Hamilton-Smith <hs.jason@gmail.com>
2018-07-14 13:18:35 -0700

    Better board management

--- board.py
+++ board.py
@@ -6,68 +6,87 @@ class SnakebirdBoardError(Exception):
     """Raised when there's a validaiton error with the board"""
 
 
+COLOR_RST = '\033[39m'
+COLOR_RED = '\033[31m'
+COLOR_BLU = '\033[32m'
+COLOR_GRN = '\033[33m'
+COLOR_YEL = '\033[34m'
+COLOR_MAG = '\033[35m'
+COLOR_CYA = '\033[36m'
+
+
+# Board table, contains:
+# element_id, symbol, fancy_symbol, color
+# Symbols are amtched in order fist to last
+# So snake heads have to come before bodies
+boardtable = [
+    ('space',       ' ', ' ',      None),
+    ('solid',       '#', '\u2588', None),
+    ('spike',       '+', '\u271A', None),
+    ('snake red 0', 'R', '\u263A', COLOR_RED),
+    ('snake grn 0', 'G', '\u263A', COLOR_GRN),
+    ('snake blu 0', 'B', '\u263A', COLOR_BLU),
+    ('snake red',   'r', '\u25CF', COLOR_RED),
+    ('snake grn',   'g', '\u25CF', COLOR_GRN),
+    ('snake blu',   'b', '\u25CF', COLOR_BLU),
+    ('block 1',     '1', '\u25A1', COLOR_CYA),
+    ('block 2',     '2', '\u25A1', COLOR_YEL),
+    ('block 3',     '3', '\u25A1', COLOR_MAG),
+    ('block 4',     '4', '\u25A1', COLOR_BLU),
+    ('block 5',     '5', '\u25A1', COLOR_GRN),
+    ('fruit',       'F', '\u2764', None),
+        ]
+
+# Specials table, teleport and endpoint
+specials = [
+    ('teleport',     'X', '\u2609', None),
+    ('endpoint',     'O', '\u269D', None),
+]
+specials_lut = {s[0]: s[1:] for s in specials}
+
+# Table of characters to completely ignore
+ignore = ['\n']
+
+
 def load_board(fname):
     board = []
     teleports = []
     endpoint = None
     rowlen = 0
-    heads = []
     with open(fname, 'r') as f:
         for y, line in enumerate(f):
             row = []
             board.append(row)
+            print('')
             for x, char in enumerate(line):
-                if char == ' ':
-                    row.append('space')
-                elif char == '#':
-                    row.append('solid')
-                elif char == '+':
-                    row.append('spike')
-                elif char == 'R':
-                    row.append('snake red 0')
-                    heads.append((x, y))
-                elif char == 'G':
-                    row.append('snake grn 0')
-                    heads.append((x, y))
-                elif char == 'B':
-                    row.append('snake blu 0')
-                    heads.append((x, y))
-                elif char == 'r':
-                    row.append('snake red')
-                elif char == 'g':
-                    row.append('snake grn')
-                elif char == 'b':
-                    row.append('snake blu')
-                elif char == '1':
-                    row.append('block 1')
-                elif char == '2':
-                    row.append('block 2')
-                elif char == '3':
-                    row.append('block 3')
-                elif char == '4':
-                    row.append('block 4')
-                elif char == '5':
-                    row.append('block 5')
-                elif char == 'F':
-                    row.append('fruit')
-                elif char == 'X':
-                    teleports.append((y,x))
-                    row.append('space')
-                elif char == 'O':
-                    row.append('space')
-                    if endpoint is not None:
-                        raise SnakebirdBoardError("Multiple Endpoints")
-                    endpoint = (y, x)
-                elif char == '\n':
-                    continue
+                for spc in specials:
+                    if spc[1] == char:
+                        row.append('space')
+                        if spc[0] == 'teleport':
+                            teleports.append((y,x))
+                        if spc[0] == 'endpoint':
+                            if endpoint is not None:
+                                raise SnakebirdBoardError("Multiple Endpoints")
+                            endpoint = (y, x)
+                        break
                 else:
-                    raise SnakebirdBoardError(f"Unknown char '{char}'")
-            if rowlen != 0 and len(row) != rowlen:
-                raise SnakebirdBoardError("Ragged array for level!")
+                    for brd in boardtable:
+                        if brd[1] == char:
+                            print(brd[0], end=' ')
+                            row.append(brd[0])
+                            break
+                    else:
+                        if char not in ignore:
+                            raise SnakebirdBoardError(f"Unknown char '{char}'")
             rowlen = len(row)
-    numrows = len(board)
+        numrows = len(board)
 
-    for x, y in heads:
+    heads = []
+    for y, line in enumerate(board):
+        for x, elem in enumerate(line):
+            if elem in['snake red 0', 'snake grn 0', 'snake blu 0']:
+                heads.append((y,x))
+    for y, x in heads:
         snake = board[y][x][6:9]
         index = 0
         while True:
@@ -92,64 +111,26 @@ def load_board(fname):
     return board, teleports, endpoint
 
 
-COLOR_RST = '\033[39m'
-COLOR_RED = '\033[31m'
-COLOR_BLU = '\033[32m'
-COLOR_GRN = '\033[33m'
-COLOR_YEL = '\033[34m'
-COLOR_MAG = '\033[35m'
-COLOR_CYA = '\033[36m'
-
 
 def draw_board(board, teleports, endpoint, color=True):
     for y, row in enumerate(board):
         rowchars = []
         for x, elem in enumerate(row):
-            elclass = elem[0:5]
-            if elclass == 'space':
+            if elem == 'space':
                 if endpoint == (y, x):
-                    rowchars.append('\u269D')
+                    rowchars.append(specials_lut['endpoint'][1])
+                    continue
                 elif (y,x) in teleports:
-                    rowchars.append('\u2609')
-                else:
-                    rowchars.append(' ')
-            elif elclass == 'solid':
-                rowchars.append('\u2588')
-            elif elclass == 'spike':
-                rowchars.append('\u271A')
-            elif elclass == 'fruit':
-                rowchars.append('\u2764')
-            elif elclass == 'snake':
-                clr = elem[6:9]
-                if color:
-                    if clr == 'red':
-                        rowchars.append(COLOR_RED)
-                    elif clr == 'grn':
-                        rowchars.append(COLOR_BLU)
-                    elif clr == 'blu':
-                        rowchars.append(COLOR_GRN)
-                if elem[10] == '0':
-                    rowchars.append('\u263A')
-                else:
-                    rowchars.append('\u25CF')
-                if color:
-                    rowchars.append(COLOR_RST)
-            elif elclass == 'block':
-                num = elem[6]
-                if color:
-                    if num == '1':
-                        rowchars.append(COLOR_CYA)
-                    elif num == '2':
-                        rowchars.append(COLOR_MAG)
-                    elif num == '3':
-                        rowchars.append(COLOR_YEL)
-                    elif num == '4':
-                        rowchars.append(COLOR_GRN)
-                    elif num == '5':
-                        rowchars.append(COLOR_BLU)
-                rowchars.append('\u25A1')
-                if color:
-                    rowchars.append(COLOR_RST)
+                    rowchars.append(specials_lut['teleport'][1])
+                    continue
+            for brd in boardtable:
+                if elem.startswith(brd[0]):
+                    if color and brd[3]:
+                        rowchars.append(brd[3])
+                    rowchars.append(brd[2])
+                    if color and brd[3]:
+                        rowchars.append(COLOR_RST)
+                    break
         print(''.join(rowchars))
 
 
