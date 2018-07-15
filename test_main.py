cd88328eaa25f76bbd9006de94eda0206867ef15
Jason Hamilton-Smith <hs.jason@gmail.com>
2018-07-15 01:27:17 -0700

    Add more tests

--- test_main.py
+++ test_main.py
@@ -1,8 +1,9 @@
 import pytest
 from textwrap import dedent
-from game import IllegalMove, InvalidMove, UnsafeMove, MissionComplete
+from game import IllegalMove, UnsafeMove, MissionComplete
 from game import move_board_state
 from board import load_board, draw_board
+
 
 def brd(board):
     return dedent(board).strip()
@@ -50,5 +51,44 @@ def test_move_blocked():
     """)
 
     with pytest.raises(IllegalMove):
+        result = execute(board, "rd")
+    assert result is None
+
+
+def test_finish():
+    result = None
+    board = brd("""
+        _rrrRO_
+        #######
+    """)
+    with pytest.raises(MissionComplete):
+        result = execute(board, "rd")
+    assert result is None
+
+
+def test_basic_fall():
+    result = None
+    board = brd("""
+        __rrR__
+        ###____
+        #######
+    """)
+    result = execute(board, "rd")
+    desired = brd("""
+        _______
+        ###rrR_
+        #######
+    """)
+    assert result == desired
+
+
+def test_spikes_fall():
+    result = None
+    board = brd("""
+        __rrR__
+        ###____
+        +++++++
+    """)
+    with pytest.raises(UnsafeMove):
         result = execute(board, "rd")
     assert result is None
